41514a986eb8187ad997096b1a8a6912c930f121
CerrutiGH <bielhcsousa@gmail.com>
2021-10-28 12:02:21 -0300

    Add css and images | Add InputCustomer.cs | Add View Home and Layout configured

---
+++ ClassLibraryModels/InputModels/InputCustomer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryModels.InputModels
+{
+    public class InputCustomer
+    {
+        [Key]
+        public int code { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome completo é obrigatório")]
+        [Display(Name = "Nome completo")]
+        public string name { get; set; }
+
+        [Required(ErrorMessage = "O campo Sexo é obrigatório")]
+        [Display(Name = "Sexo")]
+        public char sex { get; set; }
+
+        [Required(ErrorMessage = "O campo Email é obrigatório")]
+        [Display(Name = "Email")]
+        public string email { get; set; }
+
+        [Required(ErrorMessage = "O campo Senha é obrigatório")]
+        [Display(Name = "Senha")]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+
+        [Required(ErrorMessage = "O campo Confirmar senha é obrigatório")]
+        [Display(Name = "Confirmar senha")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+
+        [Required(ErrorMessage = "O campo Data de nascimento é obrigatório")]
+        [Display(Name = "Data de nascimento")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime birthday { get; set; }
+
+
+        [Required(ErrorMessage = "O campo CPF é obrigatório")]
+        [Display(Name = "CPF")]
+        public string cpf { get; set; }
+
+
+        [Required(ErrorMessage = "O campo Telefone é obrigatório")]
+        [Display(Name = "Telefone")]
+        public string telephone { get; set; }
+    }
+}
--- LotusProject/App_Start/RouteConfig.cs
+++ LotusProject/App_Start/RouteConfig.cs
@@ -14,9 +14,9 @@ namespace LotusProject
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
+                name: "HomeRoute",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Home", id = UrlParameter.Optional }
             );
         }
     }
