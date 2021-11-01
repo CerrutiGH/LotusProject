4dd6d70d0c69cd29fd1b02096f1f06b4bdff4b17
CerrutiGH <bielhcsousa@gmail.com>
2021-11-01 19:36:24 -0300

    Button for MainStore

--- LotusProject/Controllers/CustomerController.cs
+++ LotusProject/Controllers/CustomerController.cs
@@ -18,20 +18,13 @@ namespace LotusProject.Controllers
         public ActionResult SignIn(InputLoginCustomer customer)
         {
             //var Sign = custact.SignIn(customer);
-            LotusContext db = new LotusContext();
-            var login = db.Customers.Where(x => x.Email.Equals(customer.email) && x.password.Equals(customer.password)).FirstOrDefault();
-            if (login != null)
-            {
-                Session["EmailLog"] = login.Email.ToString();
-                return RedirectToAction("Home", "Home");
-            }
+          
             return RedirectToAction("About", "Home");
         }
 
 
         public ActionResult Logout()
         {
-            Session["emailSignIn"] = null;
             return RedirectToAction("Home", "Home");
         }
     }
