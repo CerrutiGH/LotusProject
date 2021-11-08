c325fcd5a34e9e457207fa5250a416513d7943eb
3aetim <3aetim@LAB3PC10>
2021-11-08 12:01:02 -0300

    Add ClassLibraries | AdmCenterEmployee

---
+++ ClassLibraryDatabaseActions/ActionsCustomer/ActionCustomer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryDatabaseActions.ActionsCustomer
+{
+    public class ActionCustomer
+    {
+
+
+        public Employee SignInEmployee(Employee employee)
+        {
+            CommandMySQL = new MySqlCommand("select * from tbEMPLOYEE where EMPLOGIN = @login and EMPPASSWORD = @password", connection.ConnectBD());
+            CommandMySQL.Parameters.Add("@login", MySqlDbType.VarChar).Value = employee.login;
+            CommandMySQL.Parameters.Add("@password", MySqlDbType.VarChar).Value = employee.password;
+
+            MySqlDataReader ReaderMySQL;
+            ReaderMySQL = CommandMySQL.ExecuteReader();
+
+            if (ReaderMySQL.HasRows)
+            {
+                while (ReaderMySQL.Read())
+                {
+                    Employee emp = new Employee();
+                    {
+                        emp.login = Convert.ToString(ReaderMySQL["EMPLOGIN"]);
+                        emp.password = Convert.ToString(ReaderMySQL["EMPPASSWORD"]);
+                    }
+                }
+            }
+            else
+            {
+                employee.login = null;
+                employee.password = null;
+            }
+            return employee;
+        }
+
+    }
+}
