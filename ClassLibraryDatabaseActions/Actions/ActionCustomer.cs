dd25668658147ef03b7bdb4179b036f43c2aa4f1
CerrutiGH <bielhcsousa@gmail.com>
2021-11-16 20:24:31 -0300

    LogIn Customer done | Register Customer Add

---
+++ ClassLibraryDatabaseActions/Actions/ActionCustomer.cs
@@ -0,0 +1,58 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibraryConnection;
+using ClassLibraryModels.InputModels;
+using ClassLibraryModels.Models;
+
+namespace ClassLibraryDatabaseActions.ActionsCustomer
+{
+
+
+    public class ActionCustomer
+    {
+        Connection conn = new Connection();
+        MySqlCommand Cmd = new MySqlCommand();
+
+
+        public string SelectEmail(string varEMail)
+        {
+
+            Cmd = new MySqlCommand("CALL spSelectEmail(@Email);", conn.ConnectBD());
+            Cmd.Parameters.Add("@Email", MySqlDbType.VarChar).Value = varEMail;
+            string Email = (string)Cmd.ExecuteScalar();
+
+            if (Email == null)
+            {
+                Email = "";
+            }
+            return Email;
+        }
+
+
+        public Customer SignIn(string email, string pass)
+        {
+            Cmd = new MySqlCommand("CALL spLoginUser(@Email, @Password);", conn.ConnectBD());
+            Cmd.Parameters.Add("@Email", MySqlDbType.VarChar).Value = email;
+            Cmd.Parameters.Add("@Password", MySqlDbType.VarChar).Value = pass;
+
+           var dtr = Cmd.ExecuteReader();
+           var TempCust = new Customer();
+
+            if (dtr.Read())
+            {
+                TempCust.email = Convert.ToString(dtr["CustEmail"]);
+                TempCust.password = Convert.ToString(dtr["CustPassword"]);
+                TempCust.name = Convert.ToString(dtr["CustName"]);
+            }
+            dtr.Close();
+            conn.DisconnectBD();
+            return TempCust;
+
+
+        }
+    }
+}
--- LotusProject/Controllers/CustomerController.cs
+++ LotusProject/Controllers/CustomerController.cs
@@ -1,5 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Web.Mvc;
+using System.Web.Security;
+using ClassLibraryDatabaseActions.ActionsCustomer;
 using ClassLibraryModels.InputModels;
 using ClassLibraryModels.ViewModels;
 
@@ -8,21 +11,74 @@ namespace LotusProject.Controllers
 {
     public class CustomerController : Controller
     {
+        ActionCustomer custumer = new ActionCustomer();
         [HttpPost]
         public ActionResult InsertCustomer()
         {
             return View();
         }
 
-        [HttpPost]
-        public ActionResult SignIn()
+        public ActionResult ViewLoginCustomer()
         {
-            return RedirectToAction("About", "Home");
+            return View();
+        }
+
+        public ActionResult ViewRegisterCustomer()
+        {
+            return View();
+        }
+
+
+        public ActionResult CheckEmail(string email)
+        {
+            bool EmailExists;
+
+            var customer = new ActionCustomer();
+            string login = customer.SelectEmail(email);
+
+            if (login.Length == 0)
+            {
+                EmailExists = false;
+            }
+            else
+            {
+                EmailExists = true;
+            }
+
+            return Json(!EmailExists, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpPost]
+        public ActionResult SignIn(InputLoginCustomer login)
+        {
+           
+            if (!ModelState.IsValid) {
+                return View(login);
+            }
+            ViewBag.CustumerAttributes = custumer.SignIn(login.email, login.password);
+
+            if (ViewBag.CustumerAttributes.email != null && ViewBag.CustumerAttributes.password != null)
+            {
+                FormsAuthentication.Encrypt(new FormsAuthenticationTicket(1, ViewBag.CustumerAttributes.email, DateTime.Now, DateTime.Now.AddHours(12), true, ViewBag.CustumerAttributes.password));
+                Session["emailCust"] = ViewBag.CustumerAttributes.email;
+                Session["nameCust"] = ViewBag.CustumerAttributes.name;
+
+
+                return RedirectToAction("Home", "Home");
+            }
+            else
+            {
+                return RedirectToAction("ViewLoginCustomer", "Customer");
+            }
         }
 
 
         public ActionResult Logout()
         {
+            Session["emailCust"] = null;
+            Session["nameCust"] = null;
+
             return RedirectToAction("Home", "Home");
         }
     }
