82aa5af7d829e3c0e07aad06ed0bd1e991c22f66
CerrutiGH <bielhcsousa@gmail.com>
2021-11-20 08:31:59 -0300

    Add some procedures for transaction

--- Database/Database-LotusSPA.sql
+++ Database/Database-LotusSPA.sql
@@ -3,21 +3,9 @@ CREATE DATABASE IF NOT EXISTS dbLotus;
 
 USE dbLotus;
 
-CREATE TABLE IF NOT EXISTS tbUF(
-UFCode int primary key auto_increment,
-UF char(2) not null
-);
 
-CREATE TABLE IF NOT EXISTS tbCity(
-CityCode int auto_increment primary key,
-CityName varchar(100) not null,
-UFCode int not null references tbUF(UFCode) on update cascade -- Verificado terceiro
-);
-
-CREATE TABLE IF NOT EXISTS tbAddress(
-CEP varchar(10) primary key not null,
-Address varchar(200) not null,
-CityCode int not null references tbCity(CityCode) on update cascade -- Verificado segundo
+CREATE TABLE IF NOT EXISTS tbCep(
+CEP varchar(10) primary key not null
 );
 
 
@@ -30,8 +18,8 @@ CustTel varchar(12) not null unique,
 CustEmail varchar(150) not null unique,
 CustPassword varchar(18) not null,
 CustNumberAddress int not null,
-IsDeleted char(3) not null default 'No',
-CEPAddress varchar(10) not null references tbAddress(CEP) on update cascade -- Verificado primeiro
+IsDeleted char(1) not null default 'N', -- N = No | Y = Yes
+CEPAddress varchar(10) not null references tbCep(CEP) on update cascade -- Verificado primeiro
 );
 
 CREATE TABLE IF NOT EXISTS tbRole(
@@ -50,18 +38,15 @@ EmpEmail varchar(150) not null unique,
 EmpPassword varchar(18) not null,
 EmpNumberAddress int not null,
 EmpRole int not null references tbRole(RoleCode) on update cascade,
-IsDeleted char(3) not null default 'No',
+IsDeleted char(1) not null default 'N', -- N = No | Y = Yes
 CEPAddress varchar(10) not null references tbAddress(CEP) on update cascade -- Verificado primeiro
 );
 
-CREATE TABLE IF NOT EXISTS tbPayment(
-PayCode int primary key auto_increment,
-PayOption varchar(20) not null unique,
-PayDate datetime not null default current_timestamp,
-PayValue decimal(8,2) not null,
-IsDeleted char(3) not null default 'No',
-CustCPF varchar(14) not null references tbCustomer(CustCPF) on update cascade
-);
+
+
+
+
+
 
 CREATE TABLE IF NOT EXISTS tbPackage(
 PackCode int primary key auto_increment,
@@ -75,7 +60,7 @@ ResCode int primary key auto_increment,
 ResValidity datetime not null,
 ResPrice decimal(8,2) not null,
 ResAmount int not null,
-IsDeleted char(3) not null default 'No',
+IsDeleted char(1) not null default 'N', -- N = No | Y = Yes
 CustCPF varchar(14) not null references tbCustomer(CustCPF) on update cascade,
 PackCode int not null references tbPackage(PackCode) on update cascade,
 PayCode int not null references tbpayment(PayCode) on update cascade
@@ -90,49 +75,61 @@ CREATE TABLE IF NOT EXISTS tbProduct(
 ProdBarCode int primary key,
 ProdName varchar(100) not null,
 ProdPrice decimal(8,2) not null,
-IsDeleted char(3) not null default 'No',
+IsDeleted char(1) not null default 'N', -- N = No | Y = Yes
 CatCode int not null references tbCategoryProd(CatCode) on update cascade
 );
+
 
 CREATE TABLE IF NOT EXISTS tbOrder(
 OrdCode int primary key auto_increment,
 OrdDate datetime not null default current_timestamp,
-OrdTotalPrice decimal(8,2) not null,
-IsDeleted char(3) not null default 'No',
+OrdTotalPrice decimal(8,2) not null default 0,
+StatusOrder char(1) not null default 'U', -- U = Unfinished | F = Finished 
+IsDeleted char(1) not null default 'N', -- N = No | Y = Yes
+PayCode int null references tbpayment(PayCode) on update cascade,
 CustCPF varchar(14) not null references tbCustomer(CustCPF) on update cascade
 );
+
 
 CREATE TABLE IF NOT EXISTS tbOrderItem(
 OrdCode int not null references tbOrder(OrdCode) on update cascade,
 ProdBarCode int not null references tbProduct(ProdBarCode) on update cascade,
 ItemUnitaryPrice decimal(8,2) not null,
 ItemAmount int not null,
-ItemTotalPrice decimal(8,2) not null
+ItemTotalPrice decimal(18,2) not null
+);
+
+CREATE TABLE IF NOT EXISTS tbOptionPay(
+OpPayCode int primary key auto_increment,
+PayOption varchar(20) not null unique
+);
+
+
+CREATE TABLE IF NOT EXISTS tbPayment(
+PayCode int primary key auto_increment,
+PayDate datetime not null default current_timestamp,
+PayValue decimal(8,2) not null,
+IsDeleted char(1) not null default 'N', -- N = No | Y = Yes
+StatusPayment char(1) not null default 'U', -- U = Unfinished | F = Finished
+OpPayCode varchar(20) not null references tbOptionPay(OpPayCode) on update cascade,
+CustCPF varchar(14) not null references tbCustomer(CustCPF) on update cascade
 );
 
   
-            
-SELECT * FROM tbCustomer;
-SELECT * FROM tbEmployee;
-SELECT * FROM tbAddress;
-SELECT * FROM tbCity;
-SELECT * FROM tbUF;
-SELECT * FROM tbReserve;
+SELECT * FROM tbCep;            -- Used     
+SELECT * FROM tbRole;           -- Used 
+SELECT * FROM tbCustomer;       -- Done
+SELECT * FROM tbEmployee;       -- Done
+
+SELECT * FROM tbCategoryProd;   -- Used
+SELECT * FROM tbProduct;        -- Done
+
+SELECT * FROM tbOptionPay;
 SELECT * FROM tbPayment;
 SELECT * FROM tbPackage;
-SELECT * FROM tbProduct;
-SELECT * FROM tbOrder;
-SELECT * FROM tbOrderItem;
-SELECT * FROM tbCategoryProd;
-SELECT * FROM tbRole;
+SELECT * FROM tbReserve;
 
-
-
-
-
-
- 
-
-
+SELECT * FROM tbOrder;          -- 
+SELECT * FROM tbOrderItem;      -- Done
 
 
--- Database/Stored Procedure.sql
+++ Database/Stored Procedure.sql
@@ -1,32 +1,26 @@
-USE dbLotus;
 
 Delimiter $$
 CREATE PROCEDURE spInsertCustomer(varCustCPF varchar(14), varCustName varchar(150), varCustDtNasc datetime,
 varCustGender char(1), varCustTel varchar(12), varCustEmail varchar(150), varCustPassword varchar(18), 
-varCustNumberAddress integer, varCEPAddress varchar(10), varCityname varchar(100), varUF char(2), varAddress varchar(200))
-
+varCustNumberAddress integer, varCEP varchar(10))
 BEGIN
 
-IF NOT EXISTS (SELECT CEP FROM tbAddress WHERE CEP = varCEPAddress) THEN
-			IF NOT EXISTS (SELECT CityCode FROM tbCity WHERE CityName = varCityname) THEN
-						IF NOT EXISTS (SELECT UFCode FROM tbUF WHERE UF = varUF) THEN
-										INSERT INTO tbUF (UF) VALUES (varUF);
-						END IF;
-						INSERT INTO tbCity (CityName, UFCode) VALUES (varCityName, (SELECT UFCode FROM tbUF WHERE UF = varUF));
-			END IF;
-            INSERT INTO tbAddress (CEP, Address, CityCode) VALUES (varCEPAddress, varAddress, (SELECT CityCode FROM tbCity WHERE CityName = varCityName));
+IF NOT EXISTS (SELECT CEP FROM tbCep WHERE CEP = varCEP) THEN
+            INSERT INTO tbCep (CEP) VALUES (varCEP);
 END IF;
 
 IF NOT EXISTS (SELECT CustCPF FROM tbCustomer WHERE CustCPF = varCustCPF) THEN
 				INSERT INTO tbCustomer (CustCPF, CustName, CustDtNasc, CustGender, CustTel, CustEmail, CustPassword, CustNumberAddress, CEPAddress)
-                VALUES (varCustCPF, varCustName, varCustDtNasc, varCustGender, varCustTel, varCustEmail, varCustPassword, varCustNumberAddress, varCEPAddress);
+                VALUES (varCustCPF, varCustName, varCustDtNasc, varCustGender, varCustTel, varCustEmail, varCustPassword, varCustNumberAddress, (SELECT CEP FROM tbCep WHERE CEP = varCEP));
 END IF;
 
 END; 
 $$ DELIMITER ;
+
 /*varCustCPF, varCustName, varCustDtNasc, varCustGender, varCustTel, varCustEmail, varCustPassword, varCustNumberAddress, varCEPAddress, varCityname, varUF, varAddress*/
-CALL spInsertCustomer(44687871801, "Gabriel Cerruti", '2003-02-25', "M", 11975466558, "bielhcsousa@gmail.com", "123321", 65, 05133160, "São Paulo", "SP" ,"Rua Carolina Guedes");
-CALL spInsertCustomer(44687871822, "Gabriel Cerruti", '2003-02-25', "M", 11975466522, "bielhcsou22@gmail.com", "123322", 70, 05133122, "São Carlos", "SP" ,"Rua Homiranha");
+CALL spInsertCustomer(44687871801, "Gabriel Cerruti", '2003-02-25', "M", 11975466558, "bielhcsousa@gmail.com", "123321", 65, 05133160);
+CALL spInsertCustomer(44687871822, "Gabriel Cerruti", '2003-02-25', "M", 11975466522, "bielhcsou22@gmail.com", "123322", 70, 05133122);
+CALL spInsertCustomer(45665465444, "Fernanda Rocha", '2003-02-25', "M", 11975465522, "fefes@gmail.com", "123322", 70, 05133122);
 
 
 
@@ -48,31 +42,109 @@ $$ DELIMITER ;
 CALL spLoginUser("bielhcsou22@gmail.com", "123322");
 
 
-
-
-
+DROP PROCEDURE IF EXISTS spInsertEmployee;
 
 Delimiter $$
-CREATE PROCEDURE spInsertCustomer(varCustCPF varchar(14), varCustName varchar(150), varCustDtNasc datetime,
-varCustGender char(1), varCustTel varchar(12), varCustEmail varchar(150), varCustPassword varchar(18), 
-varCustNumberAddress integer, varCEPAddress varchar(10), varCityname varchar(100), varUF char(2), varAddress varchar(200))
-
+CREATE PROCEDURE spInsertEmployee(varEmpCPF varchar(14), varEmpName varchar(150), varEmpLogin varchar(50), varEmpDtNasc datetime,
+varEmpGender char(1), varEmpTel varchar(12), varEmpEmail varchar(150), varEmpPassword varchar(18), 
+varEmpNumberAddress integer, varCEP varchar(10), varRoleName varchar(50))
 BEGIN
 
-IF NOT EXISTS (SELECT CEP FROM tbAddress WHERE CEP = varCEPAddress) THEN
-			IF NOT EXISTS (SELECT CityCode FROM tbCity WHERE CityName = varCityname) THEN
-						IF NOT EXISTS (SELECT UFCode FROM tbUF WHERE UF = varUF) THEN
-										INSERT INTO tbUF (UF) VALUES (varUF);
-						END IF;
-						INSERT INTO tbCity (CityName, UFCode) VALUES (varCityName, (SELECT UFCode FROM tbUF WHERE UF = varUF));
-			END IF;
-            INSERT INTO tbAddress (CEP, Address, CityCode) VALUES (varCEPAddress, varAddress, (SELECT CityCode FROM tbCity WHERE CityName = varCityName));
+IF NOT EXISTS (SELECT CEP FROM tbCep WHERE CEP = varCEP) THEN
+            INSERT INTO tbCep (CEP) VALUES (varCEP);
 END IF;
 
-IF NOT EXISTS (SELECT CustCPF FROM tbCustomer WHERE CustCPF = varCustCPF) THEN
-				INSERT INTO tbCustomer (CustCPF, CustName, CustDtNasc, CustGender, CustTel, CustEmail, CustPassword, CustNumberAddress, CEPAddress)
-                VALUES (varCustCPF, varCustName, varCustDtNasc, varCustGender, varCustTel, varCustEmail, varCustPassword, varCustNumberAddress, varCEPAddress);
-END IF;
+
+IF NOT EXISTS (SELECT EmpLogin FROM tbEmployee WHERE EmpLogin = varEmpLogin) THEN
+
+				IF NOT EXISTS (SELECT EmpCPF FROM tbEmployee WHERE EmpCPF = varEmpCPF) THEN
+                
+								IF NOT EXISTS (SELECT EmpTel FROM tbEmployee WHERE EmpTel = varEmpTel) THEN
+											IF NOT EXISTS (SELECT EmpEmail FROM tbEmployee WHERE EmpEmail = varEmpEmail) THEN
+																IF NOT EXISTS (SELECT RoleCode FROM tbrole WHERE RoleName = varRoleName) THEN 
+																		INSERT INTO tbRole (RoleName) VALUES (varRoleName);
+																END IF; -- End RoleCode
+                                
+														INSERT INTO tbEmployee (EmpCPF, EmpName, EmpLogin, EmpDtNasc, EmpGender, EmpTel, EmpEmail, EmpPassword, EmpNumberAddress, EmpRole, CEPAddress)
+														VALUES (varEmpCPF, varEmpName, varEmpLogin, varEmpDtNasc, varEmpGender, varEmpTel, varEmpEmail, varEmpPassword, varEmpNumberAddress, (SELECT RoleCode FROM tbrole WHERE RoleName = varRoleName), (SELECT CEP FROM tbCep WHERE CEP = varCEP));
+                                            END IF; -- End EmpEmail
+								END IF; -- End EmpTel
+                END IF; -- End EmpCPF
+END IF; -- End EmpLogin
 
 END; 
 $$ DELIMITER ;
+
+CALL spInsertEmployee("44697971801", "Gabriel Cerruti", "Cerriti", '2003-02-25',
+"m", "1197546558", "bielhousa@gmail.com", "123321", 
+12, "02522-100", "Gerente");
+
+
+
+
+
+
+
+DELIMITER $$
+CREATE PROCEDURE spInsertProduct (varProdBarCode int, varProdName varchar(100), varProdPrice decimal(8,2), varCatName varchar(100))
+BEGIN
+IF NOT EXISTS (SELECT ProdBarCode FROM tbproduct WHERE ProdBarCode = varProdBarCode) THEN
+				IF NOT EXISTS (SELECT CatName FROM tbcategoryprod WHERE CatName = varCatName) THEN
+								INSERT INTO tbcategoryprod (CatName) VALUES (varCatName);
+				END IF;
+                
+                INSERT INTO tbProduct (ProdBarCode, ProdName, ProdPrice, CatCode) VALUES (varProdBarCode, varProdName, varProdPrice, (SELECT CatCode FROM tbCategoryProd WHERE CatName = varCatName));
+END IF;
+END; 
+$$ DELIMITER ;
+
+CALL spInsertProduct (123654656, "Celular", 50.63, "Eletronico");
+CALL spInsertProduct (123600006, "Bob esponja", 70.00, "Brinquedo");
+CALL spInsertProduct (123609999, "Mickey", 70.00, "Brinquedo");
+CALL spInsertProduct (123607999, "Mickey mouse", 70.00, "Pelucia");
+CALL spInsertProduct (555555555, "Mickey mouse", 70.00, "Pelucia");
+
+
+
+DROP PROCEDURE IF EXISTS spInsertOrderItem;
+DELIMITER $$
+CREATE PROCEDURE spInsertOrderItem (varProdBarCode int, varItemAmount int, varCustCPF varchar(14))
+BEGIN
+IF NOT EXISTS (SELECT OrdCode FROM tborder WHERE CustCPF = varCustCPF AND StatusOrder = 'U') THEN
+				INSERT INTO tborder (CustCPF) VALUES (varCustCPF);
+        END IF;
+IF NOT EXISTS (SELECT varProdBarCode FROM tbOrderItem WHERE OrdCode = (SELECT OrdCode FROM tborder WHERE CustCPF = varCustCPF AND StatusOrder = 'U') AND ProdBarCode = varProdBarCode) THEN
+		
+				INSERT INTO tbOrderItem (OrdCode, ProdBarCode, ItemUnitaryPrice, ItemAmount, ItemTotalPrice)
+                VALUES ((SELECT OrdCode FROM tborder WHERE CustCPF = varCustCPF AND StatusOrder = 'U'), varProdBarCode, (SELECT ProdPrice FROM tbproduct WHERE ProdBarCode = varProdBarCode), varItemAmount, (SELECT ProdPrice FROM tbproduct WHERE ProdBarCode = varProdBarCode));
+                
+                UPDATE tbOrderItem SET ItemTotalPrice = (varItemAmount * ItemUnitaryPrice) WHERE OrdCode = (SELECT OrdCode FROM tborder WHERE CustCPF = varCustCPF AND StatusOrder = 'U') AND ProdBarCode = varProdBarCode;
+ELSE
+
+UPDATE tbOrderItem SET ItemAmount =  varItemAmount WHERE OrdCode = (SELECT OrdCode FROM tborder WHERE CustCPF = varCustCPF AND StatusOrder = 'U') AND ProdBarCode = varProdBarCode;
+UPDATE tbOrderItem SET ItemTotalPrice = (varItemAmount * ItemUnitaryPrice) WHERE OrdCode = (SELECT OrdCode FROM tborder WHERE CustCPF = varCustCPF AND StatusOrder = 'U') AND ProdBarCode = varProdBarCode;
+END IF;
+END; 
+$$ DELIMITER ;
+
+CALL spInsertOrderItem (123609999, 2, 44687871801);
+CALL spInsertOrderItem (123600006, 2, 44687871801);
+
+
+DROP PROCEDURE IF EXISTS spFinishOrder;
+DELIMITER $$
+CREATE PROCEDURE spFinishOrder(varPayOption varchar(20), varCustCPF varchar(14))
+BEGIN
+IF NOT EXISTS (SELECT OpPayCode FROM tbOptionPay WHERE PayOption = varPayOption) THEN
+				INSERT INTO tbOptionPay (PayOption) VALUES (varPayOption);
+END IF;
+IF EXISTS (SELECT PayCode FROM tbOrder WHERE StatusOrder = 'U' AND CustCPF = varCustCPF) THEN
+				INSERT INTO tbpayment (PayValue, OpPayCode, CustCPF) VALUES ((SELECT SUM(ItemTotalPrice) FROM tbOrderItem WHERE OrdCode = (SELECT OrdCode FROM tbOrder WHERE CustCPF = varCustCPF AND StatusOrder = 'U')), (SELECT OpPayCode FROM tbOptionPay WHERE PayOption = varPayOption), varCustCPF);
+END IF;
+
+UPDATE tbOrder SET OrdTotalPrice = (SELECT PayValue FROM tbpayment WHERE CustCPF = varCustCPF AND StatusPayment = 'U'), StatusOrder = 'F', PayCode = (SELECT PayCode FROM tbpayment WHERE CustCPF = varCustCPF AND StatusPayment = 'U') WHERE CustCPF = varCustCPF AND StatusOrder = 'U';
+Update tbpayment SET StatusPayment = 'F';
+END; 
+$$ DELIMITER ;
+
+CALL spFinishOrder ("Crédito", 44687871801);
